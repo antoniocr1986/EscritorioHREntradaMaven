184ff216b6c81da388d195df35493de0ea241441
Antonio <antonicr1986@hotmail.com>
2023-10-04 02:55:44 +0200

    Añadidas clases en la carpeta modelo con sus respectivos atributos, constructores, getters y setters

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,16 @@
+import vistas.MainForm;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author antonio minero
+ */
+public class MainClass {
+
+    public static void main(String[] args) {
+        
+    MainForm ventana = new MainForm();
+    ventana.setVisible(true);
+    }
+}
---
+++ src/main/java/login/CConexion.java
@@ -0,0 +1,131 @@
+package login;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import modelo.User;
+import vistas.MainForm;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author antonio minero
+ */
+public class CConexion {
+    Connection conectar = null;
+    
+    String usuario = "admin";
+    String contraseña = "admin";
+    String bd ="HREntrada2";
+    String ip = "localhost";
+    String puerto ="5432";
+    
+    String admin = "gus";
+    
+    String connectionString = "jdbc:postgresql://"+ip+":"+puerto+"/"+bd;
+    
+    // Crear un ArrayList de objetos User
+    private static ArrayList<User> userList = new ArrayList<>();
+    
+    public Connection establecerConexion(){
+        try{
+            Class.forName("org.postgresql.Driver");
+            conectar = DriverManager.getConnection(connectionString,usuario,contraseña);
+        }catch(Exception ex){
+            JOptionPane.showMessageDialog(null,"Error al conectar a la BD, error: "+ex.toString());          
+        }     
+        return conectar;
+    }
+    
+    public boolean verificarCredenciales(String usuario, String contrasena) {
+        try {
+            // Consulta SQL para buscar un usuario con las credenciales proporcionadas
+            String sql = "SELECT * FROM users WHERE login = ? AND pass = ?";
+            PreparedStatement statement = conectar.prepareStatement(sql);
+            statement.setString(1, usuario);
+            statement.setString(2, contrasena);
+            ResultSet resultSet = statement.executeQuery();
+
+            // Si se encuentra un resultado, las credenciales son válidas
+            if (resultSet.next()) {
+                return true;
+            }
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, "Error al verificar las credenciales, error: " + ex.toString());
+        }
+        return false; // Si no se encuentra ningún resultado, las credenciales no son válidas
+    }
+    
+    public boolean isAdmin(String usuario, String contraseña){
+        if (usuario.equals(admin)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    //***GUS Método para comprobar login por sockets   
+    public void entrar(String usuario, String contraseña){
+        MainForm principal = new MainForm();
+        boolean salir = false;
+        try {
+            //IMPLEMENTA
+            Socket socket = new Socket("192.168.0.20", 5432);
+            // Scanner lectorPalabra = new Scanner(System.in);
+            BufferedReader lector = new BufferedReader(new InputStreamReader(socket.getInputStream()));//flujo lectura del server
+            BufferedWriter escriptor = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));//flujo envio al server
+            BufferedWriter asdf = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));//flujo envio al server
+            ///Llegeix del servidor el mensaje de bienvenida, y la pregunta que nos hace ///           
+
+            String mensajeServer = lector.readLine();
+            System.out.println(mensajeServer);
+            //ahora escribimos en servidor , enviandole la palabra a buscar 
+            escriptor.write(usuario);
+            escriptor.newLine();
+            escriptor.flush();
+
+            asdf.write(contraseña);
+            asdf.newLine();
+            asdf.flush();
+
+            if (usuario.equalsIgnoreCase("exit")) {
+                salir = true;
+                lector.close();
+                escriptor.close();
+                socket.close();
+
+            } else {
+                //recibimos la respuesta
+                mensajeServer = lector.readLine();
+                principal.setVisible(true);
+            }
+            socket.close();
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(Exception.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Exception.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    //***Primeros pasos para loguearme con sockets.
+    public void logInSockets(String usuario, String contraseña){
+       userList.add(new User (usuario,contraseña));
+       
+       //Ahora querremo enviar este usuario creado al logearnos por sockets al servidor
+       for (User user: userList){
+           JOptionPane.showMessageDialog(null,"Usuario creado como objeto: "+user.toString());
+           JOptionPane.showMessageDialog(null,"Nombre: "+user.getLogin()+"\nPassword: "+user.getPass());
+       }     
+    }
+}
